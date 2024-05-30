03647bc188df615b266650ab746e23488a119457
encountea <weekend-1990@mail.ru>
2024-05-30 14:05:26 +0300

    second commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,53 @@ package main
 
 import (
 	"log"
+	"os"
+
+	_ "github.com/lib/pq"
 
 	"github.com/encountea/todo-app"
 	"github.com/encountea/todo-app/pkg/handler"
 	"github.com/encountea/todo-app/pkg/repository"
 	"github.com/encountea/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("Could not initialize config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to load env: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		User: viper.GetString("db.user"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("Failed to initialize config: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	app := new(todo.Server)
 
-	log.Println("Server is running")
-	if err := app.Run("8000", handlers.InitRoutes()); err != nil {
+	log.Printf("Server is running at port: %v", viper.GetString("port"))
+	if err := app.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error to connect to server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
